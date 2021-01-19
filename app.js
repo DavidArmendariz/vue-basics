94ebb7071f39e878f54fa4f965668bc8f07e104e
David Armendariz <darmendariz1998@outlook.com>
2021-01-19 18:44:51 -0500

    Added basic vue app

---
+++ app.js
@@ -0,0 +1,10 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      courseGoal: 'Finish the course and learn vue',
+      vueLink: 'https://vuejs.org',
+    };
+  },
+});
+
+app.mount('#user-goal');
